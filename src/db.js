3fe9349250dda74671282a16fda5ed19b7dc9ef0
Hungrymikez <codemashop@gmail.com>
2025-10-20 02:47:14 -0500

    deploy supabase 2.5

--- src/db.js
+++ src/db.js
@@ -1,14 +1,11 @@
-// archivo: db.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  "https://wpcabzvsxajcuycwohpu.supabase.co",   // ← tu URL de Supabase
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwY2FienZzeGFqY3V5Y3dvaHB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MDUwNzYsImV4cCI6MjA3NTk4MTA3Nn0.sv1oMrlNcGleLxc5c9a63aTqH3P9dwyHqHboeSTJ5SI"                   // ← tu "anon key" o "service role key"
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default supabase;
-
-
 
 
 // const mysql = require('mysql2/promise');
